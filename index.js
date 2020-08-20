8784bb63c066eae1ebfaa3229ea3a89f0ef40dd4
Nick Kooy <nickkooy@ozinga.com>
2020-08-19 19:48:12 -0500

    updated the index example with goof's pb seed.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const seedStuff = require('./seedStuff');
-let seed = seedStuff.seedFromText('o&.>01!@!');
-console.log(seed);
-console.log(seedStuff.textFromSeed(seed, true));
+let seed = -1352168915; // (Goof's 16:34.56 PB)
+let textSeed = seedStuff.textFromSeed(seed, true);
+console.log(textSeed);  // It's randomized so it'll be something like D'%.....
+console.log(seedStuff.seedFromText(textSeed));
